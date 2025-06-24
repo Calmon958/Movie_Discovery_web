7858288f5248be6a5b62c2b153a0561963ccd648
wnjuguna <wm453856@gmail.com>
2025-06-24 15:32:00 +0300

    feat: update CSS import from App.css to index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { WatchlistProvider } from './context/WatchlistContext.jsx'
-import './App.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
